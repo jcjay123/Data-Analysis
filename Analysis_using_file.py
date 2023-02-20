324a38fb5e4763e136eb30c3755febb95b25fead
Jaya Chandra <113026492+jcjay123@users.noreply.github.com>
2023-02-20 19:12:41 +0530

    Update Analysis_using_file.py

--- Analysis_using_file.py
+++ Analysis_using_file.py
@@ -1,31 +1,38 @@
-students = pd.read_csv('grades.txt',delimiter=',',header='infer')
-students.head()
-#Handling the missing values using isnull()
-students.isnull()
-#get the sum of missing values for each column like this:
-students.isnull().sum()
-students[students.isnull().any(axis=1)]
-students.StudyHours = students.StudyHours.fillna(students.StudyHours.mean())
-students
-students = students.dropna(axis=0, how='any')
-students
-# Get the mean study hours using to column name as an index
-mean_study = students['StudyHours'].mean()
-
-# Get the mean grade using the column name as a property (just to make the point!)
-mean_grade = students.Grade.mean()
-# Print the mean study hours and mean grade
-print('Average weekly study hours: {:.2f}\nAverage grade: {:.2f}'.format(mean_study, mean_grade))
+import pandas as pd
+import numpy as np
+students =pd.read_csv('Grades.txt')
+#printing first 5 values from csv file using head()
+print(students.head())
+#checking null values
+print(students.isnull())
+#counting the sum of null values for each column
+print(students.isnull().sum())
+#getting only null value positions
+print(students[students.isnull().any(axis=1)])
+#result Nan means not a number
+#now that we have sum of null values we fill those null values with mean using fillna() function
+students.StudyHours= students.StudyHours.fillna(students.StudyHours.mean())
+students.Grade=students.Grade.fillna(students.Grade.mean())
+print(students)
+#we can remove null values like this but we should do it before setting values with mean
+students=students.dropna(axis=0,how='any')
+#Getting the mean for studhours and grade column
+studyhours_mean= students.StudyHours.mean()
+grade_mean=students.Grade.mean()
+print("studying hours mean is",round(studyhours_mean,2),"Grades mean is",round(grade_mean,2))
 # Get students who studied for the mean or more hours
-students[students.StudyHours > mean_study]
-# What was their mean grade?
-students[students.StudyHours > mean_study].Grade.mean()
-passes  = pd.Series(students['Grade'] >= 60)
-students = pd.concat([students, passes.rename("Pass")], axis=1)
-students
+print(students[students.StudyHours>studyhours_mean])
+print(students[students.Name=='Rosie'])
+#calculating mean for those who studied more than mean hours
+print(students[students.StudyHours>studyhours_mean].Grade.mean())
+#assuming pass marks is 60 and creating a column pass to see which student passed
+passed_student=pd.Series(students.Grade>=60.0)
+students = pd.concat([students, passed_student.rename("Pass")], axis=1)
+print(students)
+#counting the number of students passed and failed
 print(students.groupby(students.Pass).Name.count())
-print(students.groupby(students.Pass)['StudyHours', 'Grade'].mean())
-# Create a DataFrame with the data sorted by Grade (descending)
-students = students.sort_values('Grade', ascending=False)
-# Show the DataFrame
-students
+#calculating avg hours studied by failed and passed students and also avg grades for passed and failed students
+print(students.groupby(students.Pass)["StudyHours","Grade"].mean())
+#finally sorting data
+students=students.sort_values('Grade',ascending=False)
+print(students)
